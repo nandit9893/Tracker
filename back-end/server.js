b48d3cec0cb66501480c4c39bec795277ca2e915
Nandit <nanditsharma063@gmail.com>
2024-10-16 17:56:01 +0530

    commit

--- back-end/server.js
+++ back-end/server.js
@@ -61,7 +61,7 @@ app.get('/api/dashboard', (req, res) => {
 
 app.use('/api', router);
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
